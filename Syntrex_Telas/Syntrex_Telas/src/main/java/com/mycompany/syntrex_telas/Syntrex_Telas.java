34065260f1a64adba35f88987e362871fd66372a
gustavoprogramador37 <gustavoblazechi@gmail.com>
2025-06-23 21:02:26 -0300

    Add files via upload

---
+++ Syntrex_Telas/Syntrex_Telas/src/main/java/com/mycompany/syntrex_telas/Syntrex_Telas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.syntrex_telas;
+
+/**
+ *
+ * @author g.blazechi
+ */
+public class Syntrex_Telas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
